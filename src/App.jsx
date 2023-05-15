06ae8ed1b519727e384a0a0e447e87e585217220
Sergio-TG <sergiotorresguaymasi@gmail.com>
2023-05-15 01:47:20 -0300

    portfolio-stg

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Sidenav from './components/Sidenav'
+
+function App() {
+
+  return (
+    
+    <div>
+      <Sidenav />
+    </div>
+
+  )
+}
+
+export default App
